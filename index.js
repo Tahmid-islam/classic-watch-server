3cfec33b7efd48d67aa50b31677929eeb2cd391e
Md. Ahanaf Tahmid Islame <tahmid231@gmail.com>
2021-11-10 20:17:24 +0600

    database connected successfully

--- index.js
+++ index.js
@@ -11,10 +11,29 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nsljh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("classicWatch");
+    const productsCollection = database.collection("products");
+    console.log("Database Connected Successfully");
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Classic Watch Server Running On Port`, port);
 });
